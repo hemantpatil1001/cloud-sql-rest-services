23031494e5cb012960f0f7857da15c677a2cfd66
hemantpatil1001 <hemantpatil1001@gmail.com>
2020-11-22 16:38:43 -0500

    Initial Code

---
+++ dbaccess/src/main/java/com/business/sales_track/dbaccess/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.business.sales_track.dbaccess;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DbaccessApplication.class);
+	}
+
+}
---
+++ dbaccess/src/test/java/com/business/sales_track/dbaccess/DbaccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.sales_track.dbaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbaccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
